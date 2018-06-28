b41021453f55753d0c85cfe08718e3c030b5fab9
Haoshenqi <shenqivpn@yahoo.com>
2018-06-29 00:04:47 +0800

    Begin

--- 2.py
+++ 2.py
@@ -1,63 +1,64 @@
-#!C:/Python27
-# coding=utf-8
+import sys
 
-import os
+from _winreg import *
 
-from PIL import Image
-from pytesser import *
-
-
-# import hashlib
-
-def getGray(image_file):
-    tmpls = []
-    for h in range(0, image_file.size[1]):  # h
-        for w in range(0, image_file.size[0]):  # w
-            tmpls.append(image_file.getpixel((w, h)))
-
-    return tmpls
+# tweak as necessary 
+version = sys.version[:3]
+installpath = sys.prefix
+regpath = "SOFTWARE\\Python\\Pythoncore\\%s\\" % (version)
+installkey = "InstallPath"
+pythonkey = "PythonPath"
+pythonpath = "%s;%s\\Lib\\;%s\\DLLs\\" % (
+    installpath, installpath, installpath
+)
 
 
-def getAvg(ls):  # 获取平均灰度值
-    return sum(ls) / len(ls)
+def RegisterPy():
+    print "begin RegisterPy "
+    try:
+        print "open key : %s" % regpath
+        reg = OpenKey(HKEY_CURRENT_USER, regpath)
+    except EnvironmentError as e:
+        try:
+            reg = CreateKey(HKEY_CURRENT_USER, regpath)
+            SetValue(reg, installkey, REG_SZ, installpath)
+            SetValue(reg, pythonkey, REG_SZ, pythonpath)
+            CloseKey(reg)
+        except:
+            print "*** EXCEPT: Unable to register!"
+            return
+
+        print "--- Python", version, "is now registered!"
+        return
+
+    if (QueryValue(reg, installkey) == installpath and
+            QueryValue(reg, pythonkey) == pythonpath):
+        CloseKey(reg)
+        print "=== Python", version, "is already registered!"
+        return CloseKey(reg)
+
+    print "*** ERROR:Unable to register!"
+    print "*** REASON:You probably have another Python installation!"
 
 
-def getMH(a, b):  # 比较100个字符有几个字符相同
-    dist = 0;
-    for i in range(0, len(a)):
-        if a[i] == b[i]:
-            dist = dist + 1
-    return dist
+def UnRegisterPy():
+    # print "begin UnRegisterPy "
+    try:
+        print "open HKEY_CURRENT_USER key=%s" % (regpath)
+        reg = OpenKey(HKEY_CURRENT_USER, regpath)
+        # reg = OpenKey(HKEY_LOCAL_MACHINE, regpath)
+    except EnvironmentError:
+        print "*** Python not registered?!"
+        return
+    try:
+        DeleteKey(reg, installkey)
+        DeleteKey(reg, pythonkey)
+        DeleteKey(HKEY_LOCAL_MACHINE, regpath)
+    except:
+        print "*** Unable to un-register!"
+    else:
+        print "--- Python", version, "is no longer registered!"
 
 
-def getImgHash(fne):
-    image_file = Image.open(fne)  # 打开
-    image_file = image_file.resize((12, 12))  # 重置图片大小我12px X 12px
-    image_file = image_file.convert("L")  # 转256灰度图
-    Grayls = getGray(image_file)  # 灰度集合
-    avg = getAvg(Grayls)  # 灰度平均值
-    bitls = ''  # 接收获取0或1
-    # 除去变宽1px遍历像素
-    for h in range(1, image_file.size[1] - 1):  # h
-        for w in range(1, image_file.size[0] - 1):  # w
-            if image_file.getpixel((w, h)) >= avg:  # 像素的值比较平均值 大于记为1 小于记为0
-                bitls = bitls + '1'
-            else:
-                bitls = bitls + '0'
-    return bitls
-
-
-'''
-   m2 = hashlib.md5()
-   m2.update(bitls)
-   print m2.hexdigest(),bitls
-   return m2.hexdigest()
-'''
-
-a = getImgHash(".//testpic//001n.bmp")  # 图片地址自行替换
-files = os.listdir(".//testpic")  # 图片文件夹地址自行替换
-for file in files:
-    b = getImgHash(".//testpic//" + str(file))
-    compare = getMH(a, b)
-    print
-    file, u'相似度', str(compare) + '%'
+if __name__ == "__main__":
+    RegisterPy()
--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+# -*- coding: utf-8 -*-
 from PIL import ImageGrab
 from PIL import Image
 import math
+import time
 import operator
 from functools import reduce
 
-def likeCoefficient(pic1,pic2):
+import press
+
+home = "img/home.png"
+returnBack = "img/returnBack.png"
+confirm = "img/confirm.png"
+cancel = "img/cancel.png"
+returnPoint = {770, 350, 1150, 700}
+confirmPoint = {0, 354, 1920, 726}
+
+def likeCoefficient(pic1, pic2):
     image1 = Image.open(pic1)
     image3 = Image.open(pic2)
     # 把图像对象转换为直方图数据，存在list h1、h2 中
@@ -12,23 +23,36 @@ def likeCoefficient(pic1,pic2):
     h2 = image3.histogram()
     result = math.sqrt(reduce(operator.add, list(map(lambda a, b: (a - b) ** 2, h1, h2))) / len(h1))
     return result
-def screen():
+
+
+def screen(lu, ru, ld, rd):
     # 参数说明
     # 第一个参数 开始截图的x坐标
     # 第二个参数 开始截图的y坐标
     # 第三个参数 结束截图的x坐标
     # 第四个参数 结束截图的y坐标
-    bbox = (0, 0, 1920, 1080)
+    bbox = (lu, ru, ld, rd)
     im = ImageGrab.grab(bbox)
 
     # 参数 保存截图文件的路径
-    im.save('as.png')
-def compare():
-    return
+    im.save('as2.png')
 
 
-#开始游戏
-#返回大厅
-#确认
-#Esc
-#重启大厅
+time.sleep(10)
+screen(0, 354, 1920, 726)
+press.getCurPos()
+# 开始游戏
+# 返回大厅
+# 确认
+# Esc
+# 重启大厅
+
+# i = 0
+# while i < 10:
+#     time.sleep(5)
+#     print("开始")
+#     press.start()
+#     time.sleep(20)
+#     print("退出")
+#     press.cancel()
+#     time.sleep(10)
